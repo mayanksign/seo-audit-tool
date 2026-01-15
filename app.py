f4113fcdd6c23fc243ea951185d1a06d62cc9f5e
mayanksign <142021070+mayanksign@users.noreply.github.com>
2026-01-16 00:25:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -178,7 +178,7 @@ def analyze(domain):
         data["title"] = soup.title.string.strip() if soup.title else "NA"
 
         meta = soup.find("meta", attrs={"name": "description"})
-        data["meta"] = meta["content"][:60] if meta else "NA"
+        data["meta"] = meta["content"][:160] if meta else "NA"
 
         text = soup.get_text()
         data["words"] = len(text.split())
