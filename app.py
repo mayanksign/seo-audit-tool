809c372d305bf60f287fc09de0a5fd643b2bb49c
mayanksign <142021070+mayanksign@users.noreply.github.com>
2026-04-02 18:06:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,12 +9,10 @@ HTML = """
 <!DOCTYPE html>
 <html>
 <head>
-<head>
   <title>Free SEO Audit Tool Online | Check Website SEO Score Instantly</title>
 
   <meta name="description" content="Analyze your website SEO instantly with our free SEO audit tool. Get detailed reports, errors, and suggestions without signup.">
 
-</head>
 <style>
 * {
     margin: 0;
