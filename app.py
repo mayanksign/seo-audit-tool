cdbbcc99f0ab74785feb05e234772d49860dbefb
mayanksign <142021070+mayanksign@users.noreply.github.com>
2026-04-03 16:29:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ HTML = """
 <html>
 <head>
 
-<link rel="icon" href="/favicon.ico">
+<link rel="icon" href="/favicon.ico?v=2">
 <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
