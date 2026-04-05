ee84bb9a62dc651d94d86d39c45a4047e7f8c97c
mayanksign <142021070+mayanksign@users.noreply.github.com>
2026-04-05 18:46:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -251,6 +251,10 @@ tr:hover td {
 def about():
     return render_template('about.html')
 
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 
 
 @app.route('/robots.txt')
