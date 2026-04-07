6ea2d18d5fa81bd538361e9b6c73e4a35c7cd0f1
mayanksign <142021070+mayanksign@users.noreply.github.com>
2026-04-07 18:24:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,6 +39,8 @@ HTML = """
 
 body {
     min-height: 100vh;
+    display: flex;
+    flex-direction: column;
     overflow-x: hidden;
     background: linear-gradient(135deg, #0f172a, #1e293b);
     font-family: 'Segoe UI', sans-serif;
