31143a6a9a9bacce08890b268b005d4e3850b30d
mayanksign <142021070+mayanksign@users.noreply.github.com>
2026-06-02 18:11:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -435,6 +435,11 @@ def blog2():
 def blog3():
     return render_template('blog3.html')
 
+@app.route('/blog/on-page-seo-checklist-for-better-rankings')
+def blog4():
+    return render_template('blog4.html')
+
+
 
 
 
