125857f4e264e7c21b217c37a2eadf0fb0eb2e60
mayanksign <142021070+mayanksign@users.noreply.github.com>
2026-06-06 12:32:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1093,15 +1093,15 @@ def analyze(domain):
 
 
 
-try:
-    rob = requests.get(base + "/robots.txt", timeout=5)
-
-    if "Sitemap:" in rob.text:
+    try:
+        rob = requests.get(base + "/robots.txt", timeout=5)
+ 
+     if "Sitemap:" in rob.text:
         data["sitemap"] = "Yes"
-    else:
+     else:
         data["sitemap"] = "No"
 
-except:
+   except:
     data["sitemap"] = "Error"
       
     return data
